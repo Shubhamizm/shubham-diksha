ad06d2896ebc1d02d79410e23ded820c2ad9ed6a
shubhamizm <shubham.gupta513@gmail.com>
2024-11-03 00:44:08 +0530

    reset rsvp on submission

--- js/scripts.js
+++ js/scripts.js
@@ -253,6 +253,7 @@ $(document).ready(function () {
                     } else {
                         $('#alert-wrapper').html('');
                         $('#rsvp-modal').modal('show');
+                        $('#rsvp-form')[0].reset();
                     }
                 })
                 .fail(function (data) {
